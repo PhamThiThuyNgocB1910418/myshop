dec3b13036b09d8f2895267068e63c0be4a80f22
ThuyNgoc22 <ngocb1910418@student.ctu.edu.vn>
2022-09-17 06:33:05 -0800

    Xay dung trang chi tiet san pham

---
+++ lib/models/product.dart
@@ -0,0 +1,37 @@
+
+class Product {
+  final String? id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageURL;
+  final bool isFavorite;
+
+  Product({
+    this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageURL,
+    this.isFavorite = false,
+  });
+
+  Product copyWith({
+    String? id,
+    String? title,
+    String? description,
+    double? price,
+    String? imageURL,
+    bool? isFavorite,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      price: price ?? this.price,
+      imageURL: imageURL ?? this.imageURL,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
+  }
+}
+
